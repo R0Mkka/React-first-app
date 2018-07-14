9232ff556f7bdf5c106034e777d4fa8db5162620
Roman <alexanov.roman@mail.ru>
2018-07-15 01:13:41 +0300

    Second

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -1,13 +1,54 @@
 window.addEventListener('DOMContentLoaded', function() {
 
+  // Menu button images
+
   let menu_img = document.getElementsByTagName('img')[0];
 
-  menu_img.addEventListener('mouseover', function() {
-    menu_img.setAttribute('src', 'img/menu_focus.png');
+    menu_img.addEventListener('mouseover', function() {
+      if(menu_img.getAttribute('src') !== 'img/cross.png') {
+        menu_img.setAttribute('src', 'img/menu_focus.png');
+      }
+    });
+
+    menu_img.addEventListener('mouseout', function() {
+      if(menu_img.getAttribute('src') !== 'img/cross.png') {
+        menu_img.setAttribute('src', 'img/menu_nofocus.png');
+      }
+    });
+
+  menu_img.addEventListener('click', function() {
+    if(menu_img.getAttribute('src') === 'img/menu_focus.png') {
+      menu_img.setAttribute('src', 'img/cross.png');
+    } else {
+      menu_img.setAttribute('src', 'img/menu_focus.png');
+    }
   });
 
-  menu_img.addEventListener('mouseout', function() {
-    menu_img.setAttribute('src', 'img/menu_nofocus.png');
-  });
+  // Menu background apperance and disappearance
+
+  let preview = document.querySelector('.preview'),
+      menu = document.querySelector('.menu'),
+      preview_height,
+      height = 112;
+
+  if(preview !== null){
+    preview_height = preview.clientHeight;
+  } else {
+    preview_height = 112;
+  }
+
+  if(window.pageYOffset >= (preview_height - 112)){
+    menu.classList.add('scroll');
+  } else {
+    menu.classList.remove('scroll');
+  }
+
+  window.onscroll = function() {
+    if(window.pageYOffset >= (preview_height - 112)){
+      menu.classList.add('scroll');
+    } else {
+      menu.classList.remove('scroll');
+    }
+  }
 
 });
