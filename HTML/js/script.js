456b43aaa4ea1aadf26818e3e8ae242b64116018
Roman <alexanov.roman@mail.ru>
2018-07-26 15:49:38 +0300

    HTML is ready + a little adaptive

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -1,5 +1,27 @@
 window.addEventListener('DOMContentLoaded', function() {
 
+  // For regulating backgrond's height
+
+  let rows = document.getElementsByClassName('row'),
+      content = document.querySelector('.content'),
+      menu = document.querySelector('.menu'),
+      header = document.querySelector('.header');
+
+  let backs = document.getElementsByClassName('row__block-background'),
+      imgs = content.getElementsByTagName('img');
+
+  for(let i = 0; i < imgs.length; i++){
+      backs[i].style.height = imgs[i].offsetHeight + 'px';
+  }
+  menu.style.width = header.clientWidth + 'px';
+
+  window.onresize = function(event) {
+    for(let i = 0; i < imgs.length; i++){
+        backs[i].style.height = imgs[i].offsetHeight + 'px';
+    }
+    menu.style.width = header.clientWidth + 'px';
+  };
+
   // Menu button images
 
   let menu_img = document.getElementsByTagName('img')[0];
@@ -22,12 +44,15 @@ window.addEventListener('DOMContentLoaded', function() {
     } else {
       menu_img.setAttribute('src', 'img/menu_focus.png');
     }
+
+    $('.left-menu').toggleClass('open');
+    $('.content').toggleClass('slick');
+    $('.footer').toggleClass('slick');
   });
 
   // Menu background apperance and disappearance
 
   let preview = document.querySelector('.preview'),
-      menu = document.querySelector('.menu'),
       preview_height,
       height = 112;
 
@@ -38,17 +63,19 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 
   if(window.pageYOffset >= (preview_height - 112)){
-    menu.classList.add('scroll');
+    menu.classList.add('menu_scroll');
   } else {
-    menu.classList.remove('scroll');
+    menu.classList.remove('menu_scroll');
   }
 
   window.onscroll = function() {
     if(window.pageYOffset >= (preview_height - 112)){
-      menu.classList.add('scroll');
+      menu.classList.add('menu_scroll');
     } else {
-      menu.classList.remove('scroll');
+      menu.classList.remove('menu_scroll');
     }
   }
+
+
 
 });
